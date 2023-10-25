87bd2efa592532714944d1ed917e8347e53e2f91
Infance Tony.E <138364215+InfanceTony-cs22@users.noreply.github.com>
2023-10-25 20:32:03 +0530

    Create Differec-of-two.php

---
+++ Differec-of-two.php
@@ -0,0 +1,10 @@
+<?php
+$number1 = 10; // Replace with your first number
+$number2 = 5;  // Replace with your second number
+
+// Calculate the difference
+$difference = $number1 - $number2;
+
+// Display the result
+echo "The difference between $number1 and $number2 is: $difference";
+?>
